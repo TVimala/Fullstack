de16e8e775d691309d0cf7dadbdab42700560947
TVimala <143889003+TVimala@users.noreply.github.com>
2024-04-04 15:44:49 +0000

    add:files

---
+++ Session-5/Assignment-6.js
@@ -0,0 +1,29 @@
+let a=5
+let b=4
+console.log("Arithmetic Operations")
+console.log("Addition:"+(a+b))
+console.log("Subtraction:"+(a-b))
+console.log("Multiplication:"+(a*b))
+console.log("Division:"+(a/b))
+console.log("Remainder:"+(a%b))
+console.log("Unary Increment & Decrement")
+console.log("Increment:"+(++a))
+console.log("Decrement:"+(--b))
+console.log("Comparision")
+console.log(a == b)//false
+console.log(a != b)//true
+console.log(a < b)//false
+console.log(a > b)//true
+console.log(a <= b)//false
+console.log(a >= b)//true
+console.log("Difference b/w == & ===")
+let y='10'
+let u=10
+console.log("y==u:"+(y==u))//true
+console.log("y===u:"+(y===u))//false
+
+
+
+
+
+
